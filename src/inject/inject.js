63a5bcd9e2c546fc465e6fb76aa7586769fec974
Alex Gaynor <alex.gaynor@gmail.com>
2016-02-24 08:01:26 -0500

    It wouldn't be systemic dysfunction if it didn't affect multiple agencies!

--- src/inject/inject.js
+++ src/inject/inject.js
@@ -2,5 +2,14 @@
     /* For some reason, the SSOi servers won't take my password, because
      * the max length can only be 30. This restriction is harmful, silly
      * and entirely not enforced. Remove this from the "UI" */
-    document.getElementById("PASSWORDINPUT").removeAttribute("maxlength")
+    var el = document.getElementById("PASSWORDINPUT");
+    if (el !== null) {
+        el.removeAttribute("maxlength");
+    }
+
+    /* connect.eop.gov does the same thing for equally inexplicable reasons */
+    var els = document.getElementsByClassName("CTXMSAM_ContentFont");
+    Array.prototype.forEach.call(els, function(el) {
+        el.removeAttribute("maxlength");
+    });
 })();
